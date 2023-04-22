fc880108df5df8bea1cb69ed7482c57214f1ab76
Kaneis Zontanos <ramim@bc.edu>
2023-04-22 14:12:03 -0400

    Initial Commit

---
+++ AnimeMangaStack/AnimeMangaStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeMangaStackApp.swift
+//  AnimeMangaStack
+//
+//  Created by Kaneis Zontanos on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeMangaStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
